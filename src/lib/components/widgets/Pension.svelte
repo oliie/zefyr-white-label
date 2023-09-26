f6ece928df8c27c1c8471b9e49721f834e6f6747
Oliver Praesto <oliver.praesto@gmail.com>
2023-09-26 11:18:39 +0200

    - adds information text

--- src/lib/components/widgets/Pension.svelte
+++ src/lib/components/widgets/Pension.svelte
@@ -7,6 +7,35 @@
 </script>
 
 <Card header="Pension">
+	<div
+		tabindex="0"
+		class="mb-4 border collapse border-base-300 bg-base-200 collapse-arrow"
+		role="button"
+	>
+		<div class="min-h-0 px-6 py-2 text-sm font-bold collapse-title">Se ITP 1 nivåer</div>
+		<div class="collapse-content">
+			<table class="table">
+				<tbody>
+					<tr>
+						<td>ITP 1-nivå 40 000 kr/mån</td>
+						<th>1800 kr/mån</th>
+					</tr>
+					<tr>
+						<td>ITP 1-nivå 50 000 kr/mån</td>
+						<th>3158 kr/mån</th>
+					</tr>
+					<tr>
+						<td>ITP 1-nivå 60 000 kr/mån</td>
+						<th>6158 kr/mån</th>
+					</tr>
+					<tr>
+						<td>ITP 1-nivå 70 000 kr/mån</td>
+						<th>9158 kr/mån</th>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 	<div>
 		<input type="range" min="0" max="15000" step="200" bind:value class="range range-primary" />
 	</div>
--- src/lib/components/widgets/Savings.svelte
+++ src/lib/components/widgets/Savings.svelte
@@ -7,6 +7,10 @@
 </script>
 
 <Card header="Sparande">
+	<i class="pb-4 text-sm"
+		>Kan användas till ledighet, arbetsrelaterade inköp, löneväxling, pension, kompetensutveckling,
+		etc.</i
+	>
 	<input type="range" min="0" max="25000" step="500" bind:value class="range range-primary" />
 	<Output>
 		{formatterToSek.format(+value)}
